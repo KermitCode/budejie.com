9106b9a77880abd41f42b662eb397b015911208b
Kermit <956952515@qq.com>
2016-07-25 14:01:17 +0800

    百思不得姐的采集规则和数据库

---
+++ con_categorys.sql
@@ -0,0 +1,5 @@
+INSERT INTO `con_categorys` VALUES (1, '段子', 1, '', '', 1);
+INSERT INTO `con_categorys` VALUES (2, '图片', 1, '', '', 2);
+INSERT INTO `con_categorys` VALUES (3, '视频', 1, ' ', ' ', 3);
+INSERT INTO `con_categorys` VALUES (4, '声音', 1, ' ', ' ', 4);
+INSERT INTO `con_categorys` VALUES (5, '测试', 1, '测试测试测试', '测试测试测试测试', 15);
---
+++ con_tags.sql
@@ -0,0 +1,6 @@
+INSERT INTO `con_tags` VALUES (1, 1, '笑掉大牙', '', '', 50, 1);
+INSERT INTO `con_tags` VALUES (2, 2, '美女', '', '1', 1, 1);
+INSERT INTO `con_tags` VALUES (3, 4, '劲爆', '劲爆劲爆劲爆', '劲爆劲爆', 43, 0);
+INSERT INTO `con_tags` VALUES (4, 4, '舒缓', '1', '1', 0, 1);
+INSERT INTO `con_tags` VALUES (5, 1, '劲爆', '劲爆劲爆', '劲爆劲爆劲爆', 12, 1);
+INSERT INTO `con_tags` VALUES (6, 2, '大学生', '大学生大学生', '大学生大学生大学生', 11, 0);
